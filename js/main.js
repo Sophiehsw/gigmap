11c91b360ea35e478a9bfed84d7f3775e14d42f7
amandayg <amandaguo31@gmail.com>
2017-05-09 03:51:05 -0400

    change

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-
 var tiles = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',{
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
 });
 
 var map = L.map('map',{
   center: [22.349052, 17.396109],
-  zoom: 2
+  zoom: 2,
+  doubleClickZoom: true
 }).addLayer(tiles);
 
 /* =====================
@@ -26,23 +26,24 @@ var dataReturn;
 var list=[], list2=[],list3=[],list4=[];
 var newLine,newLine2;
 var forClear=[],forClear2=[],forClear3=[],forClear4=[];
+var markerb, marker2b;
 
 $('#alert').hide();
 
 var searchArtist = function(artistName) {
-     return $.ajax({
-      type: "GET",
-      url: "https://cors-anywhere.herokuapp.com/api.songkick.com/api/3.0/search/artists.json",
-      data: {
-        query: artistName,
-        apikey: songkick_api
-      }
-    }).done(function(data){
-      console.log(data);
-      if(data.resultsPage.totalEntries === 0 ||data.resultsPage.results.artist[0]["displayName"]==='私立恵比寿中学'){
-        $('#alert').show();
-      }
-      else{
+  return $.ajax({
+    type: "GET",
+    url: "https://cors-anywhere.herokuapp.com/api.songkick.com/api/3.0/search/artists.json",
+    data: {
+      query: artistName,
+      apikey: songkick_api
+    }
+  }).done(function(data){
+    console.log(data);
+    if(data.resultsPage.totalEntries === 0 ||data.resultsPage.results.artist[0]["displayName"]==='私立恵比寿中学'){
+      $('#alert').show();
+    }
+    else{
       console.log(data.resultsPage.results.artist[0]["displayName"]);
       artistID = data.resultsPage.results.artist[0]["id"];
       $("#choice").text(data.resultsPage.results.artist[0]["displayName"]);
@@ -59,17 +60,17 @@ var totalPage;
 //pagination
 var gettotalPage = function(artistID){
   $.ajax({
-  type: "GET",
-  url:"https://cors-anywhere.herokuapp.com/api.songkick.com/api/3.0/artists/" + artistID + "/gigography.json",
-  data: {
-    query: artistID,
-    apikey: songkick_api
-  }
-}).done(function(data){
+    type: "GET",
+    url:"https://cors-anywhere.herokuapp.com/api.songkick.com/api/3.0/artists/" + artistID + "/gigography.json",
+    data: {
+      query: artistID,
+      apikey: songkick_api
+    }
+  }).done(function(data){
 
-  totalPage = parseInt((data.resultsPage.totalEntries)/50);
-  console.log(totalPage);
-});
+    totalPage = parseInt((data.resultsPage.totalEntries)/50);
+    console.log(totalPage);
+  });
 };
 
 var getPastVenues = function(artistID){
@@ -84,30 +85,34 @@ var getPastVenues = function(artistID){
   }).done(function(data){
     if (totalPage > 1 && page < totalPage){
       page = page+1;
-    getPastVenues(artistID,totalPage);
-    console.log(page);
+      getPastVenues(artistID,totalPage);
+      console.log(page);
     }
     dataReturn = data;
-    //console.log(dataReturn);
+    console.log(dataReturn);
+    // markerb = L.circleMarker(
+    //   [dataReturn.resultsPage.results.event[0]["location"]["lat"],  dataReturn.resultsPage.results.event[0]["location"]["lng"]],
+    //   {color: "#FFB11B",fillColor: "#FFB11B",weight:2.5,fillOpacity:0.3
+    // }).bindTooltip(dataReturn.resultsPage.results.event[0]["location"]["city"] + dataReturn.resultsPage.results.event[0]["displayName"]).addTo(map);
 
-  _.map(dataReturn.resultsPage.results.event, function(venue){
+    _.map(dataReturn.resultsPage.results.event, function(venue){
       if(venue.location.lat !== null && venue.location.lng !== null){
         var marker = L.circleMarker({lat: venue.location.lat,lng: venue.location.lng} ,
           {color: "#A8D8B9", fillColor: "#A8D8B9",weight:2,fillOpacity:0.5}
-        ).bindPopup(venue.location.city + " " + venue.start.date).addTo(map);
+        ).bindTooltip(venue.location.city + " " + venue.start.date).addTo(map);
         marker.setRadius(6);
         var latlng = [venue.location.lat,venue.location.lng];
-        //console.log(latlng);
         list.push(latlng);
         //console.log(list);
         //newLine = L.polyline(list, {color: "#A8D8B9", weight: 0.5}).addTo(map);
         forClear.push(marker);//,newLine);
         //map.flyTo({lat:venue.location.lat, lng:venue.location.lng},5,0.5);
+        // window.setInterval(function() {
+        //     marker2.setLatLng(list2);
+        // },50);
 
       }
-
-}) ;
-
+    });
   });
 };
 
@@ -130,45 +135,55 @@ var getUpcomingVenues = function(artistID){
     var dataReturn2 = data;
     console.log(dataReturn2);
 
-  _.map(dataReturn2.resultsPage.results.event, function(venue){
+    _.map(dataReturn2.resultsPage.results.event, function(venue){
       if(venue.location.lat !== null && venue.location.lng !== null){
         var marker2 = L.circleMarker({lat: venue.location.lat,lng: venue.location.lng} ,
           {color: "#D0104C",fillColor: "#D0104C", weight:2.5,fillOpacity:0.3}
-        ).bindPopup(venue.location.city + " " + venue.start.date).addTo(map);  //url to songkick event page: venue.uri
+        ).bindTooltip(venue.location.city + " " + venue.start.date).addTo(map);  //url to songkick event page: venue.uri
         var latlng2 = [venue.location.lat,venue.location.lng];
         console.log(latlng2);
         list2.push(latlng2);
         console.log(list2);
         marker2.setRadius(6);
         forClear2.push(marker2);
-// window.setInterval(function() {
-//     marker2.setLatLng(list2);
-// },50);
-//
-// marker2.addTo(map);
+
       }
-});
-newLine2 = L.polyline(list2, {color: "#D0104C", weight:1}).addTo(map);
-forClear2.push(newLine2);
+    });
+    newLine2 = L.polyline(list2, {color: "#D0104C", weight:1}).addTo(map);
+    forClear2.push(newLine2);
+    marker2b = L.circleMarker([0, 0], {color: "#FFB11B",fillColor: "#FFB11B",weight:2.5,fillOpacity:0.3}).bindTooltip(dataReturn2.resultsPage.results.event[0]["location"]["city"] + " " + dataReturn2.resultsPage.results.event[0]["start"]["date"]).addTo(map);
+    marker2b.setLatLng(L.latLng(
+        list2[0][0],  list2[0][1]));
+forClear2.push(marker2b);
+        // var t = 0;
+// window.setInterval(function() {
+//     for (var i = 0; i < list2.length; i++) {
+//     marker2b.setLatLng(L.latLng(
+//         list2[i][0],
+//         list2[i][1]))};
+//     t += 0.1;
+//   }, 50);
+
   });
+
 };
 
 //2.city
 //location search
 var searchCity = function(cityName) {
-     return $.ajax({
-      type: "GET",
-      url: "https://cors-anywhere.herokuapp.com/api.songkick.com/api/3.0/search/locations.json",
-      data: {
-        query: cityName,
-        apikey: songkick_api
-      }
-    }).done(function(data){
-      console.log(data);
-      console.log(data.resultsPage.results.location[0]["metroArea"]["displayName"]);
-      cityID = data.resultsPage.results.location[0]["metroArea"]["id"];
-      $("#choice").text(data.resultsPage.results.location[0]["metroArea"]["displayName"]);
-      console.log(cityID);
+  return $.ajax({
+    type: "GET",
+    url: "https://cors-anywhere.herokuapp.com/api.songkick.com/api/3.0/search/locations.json",
+    data: {
+      query: cityName,
+      apikey: songkick_api
+    }
+  }).done(function(data){
+    console.log(data);
+    console.log(data.resultsPage.results.location[0]["metroArea"]["displayName"]);
+    cityID = data.resultsPage.results.location[0]["metroArea"]["id"];
+    $("#choice").text(data.resultsPage.results.location[0]["metroArea"]["displayName"]);
+    console.log(cityID);
   });
 };
 
@@ -190,7 +205,7 @@ var getCityevents = function(cityID){
     var dataReturn3 = data;
     console.log(dataReturn3);
 
-  _.map(dataReturn3.resultsPage.results.event, function(venue){
+    _.map(dataReturn3.resultsPage.results.event, function(venue){
       if(venue.location.lat !== null && venue.location.lng !== null){
         var marker3 = L.circleMarker({lat: venue.location.lat,lng: venue.location.lng} , {color:"#E9CD4C", fillColor: "#E9CD4C"}).bindPopup(venue.displayName).addTo(map);
         marker3.setRadius(6);
@@ -201,32 +216,32 @@ var getCityevents = function(cityID){
         console.log(list3);
         forClear3.push(marker3);
       }
-});
-});
+    });
+  });
 };
 
 //3.venue
 //venue search
 var searchVenue = function(venueName) {
-     return $.ajax({
-      type: "GET",
-      url: "https://cors-anywhere.herokuapp.com/api.songkick.com/api/3.0/search/venues.json",
-      data: {
-        query: venueName,
-        apikey: songkick_api
-      }
-    }).done(function(data){
-      console.log(data);
-      if(data.resultsPage.totalEntries === 0){
-        $('#alert').show();
-      }
-      console.log(data.resultsPage.results.venue[0]["displayName"]);
-      venueID = data.resultsPage.results.venue[0]["id"];
-      var venueMarker = L.circleMarker({lat: data.resultsPage.results.venue[0]["lat"],lng: data.resultsPage.results.venue[0]["lng"]} , {color:"#F05E1C",fillColor: "#F05E1C"}).bindPopup(data.resultsPage.results.venue[0]["displayName"]).addTo(map);
-      map.setView({lat: data.resultsPage.results.venue[0]["lat"],lng: data.resultsPage.results.venue[0]["lng"]}, 14);
-      list4.push(venueMarker);
-      $("#choice").text(data.resultsPage.results.venue[0]["displayName"]);
-      console.log(venueID);
+  return $.ajax({
+    type: "GET",
+    url: "https://cors-anywhere.herokuapp.com/api.songkick.com/api/3.0/search/venues.json",
+    data: {
+      query: venueName,
+      apikey: songkick_api
+    }
+  }).done(function(data){
+    console.log(data);
+    if(data.resultsPage.totalEntries === 0){
+      $('#alert').show();
+    }
+    console.log(data.resultsPage.results.venue[0]["displayName"]);
+    venueID = data.resultsPage.results.venue[0]["id"];
+    var venueMarker = L.circleMarker({lat: data.resultsPage.results.venue[0]["lat"],lng: data.resultsPage.results.venue[0]["lng"]} , {color:"#F05E1C",fillColor: "#F05E1C"}).bindPopup(data.resultsPage.results.venue[0]["displayName"]).addTo(map);
+    map.setView({lat: data.resultsPage.results.venue[0]["lat"],lng: data.resultsPage.results.venue[0]["lng"]}, 14);
+    list4.push(venueMarker);
+    $("#choice").text(data.resultsPage.results.venue[0]["displayName"]);
+    console.log(venueID);
   });
 };
 
@@ -244,10 +259,10 @@ var getVenueevents = function(venueID){
     var dataReturn4 = data;
     console.log(dataReturn4);
 
-        var marker4 = L.circleMarker({lat: dataReturn4.resultsPage.results.event[0]["location"]["lat"],lng: dataReturn4.resultsPage.results.event[0]["location"]["lng"]} ,
-        {color: "#D0104C"}).bindPopup(dataReturn4.resultsPage.results.event[0]["displayName"]).addTo(map);
-        forClear4.push(marker4);
-      });
+    var marker4 = L.circleMarker({lat: dataReturn4.resultsPage.results.event[0]["location"]["lat"],lng: dataReturn4.resultsPage.results.event[0]["location"]["lng"]} ,
+    {color: "#D0104C"}).bindPopup(dataReturn4.resultsPage.results.event[0]["displayName"]).addTo(map);
+    forClear4.push(marker4);
+  });
 };
 
 //past button
@@ -257,7 +272,9 @@ $("#past").click(function(e) {
   searchArtist(artistName).done(function(){
     gettotalPage(artistID);
     getPastVenues(artistID);
-});
+      // markerb.setLatLng(L.latLng(
+      //   list[0][0],  list[0][1]));
+  });
 });
 
 //future button
